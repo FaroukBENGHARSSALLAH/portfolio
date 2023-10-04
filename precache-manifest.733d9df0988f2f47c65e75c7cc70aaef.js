89436b6bc64cc44bd148b3de1de8b9f59cb3358d
Farouk' 'BEN GHARSSALLAH <farouk.bengarssallah@gmail.com>
2023-10-04 13:37:45 -0400

    Updates

---
+++ precache-manifest.733d9df0988f2f47c65e75c7cc70aaef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d396a7135f3eb5eacf01a7e479c1015",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "56be5ba291727bbbb457",
+    "url": "/portfolio/static/css/main.033c0e43.chunk.css"
+  },
+  {
+    "revision": "76bed80b140e6e6f183b",
+    "url": "/portfolio/static/js/2.7314562f.chunk.js"
+  },
+  {
+    "revision": "56be5ba291727bbbb457",
+    "url": "/portfolio/static/js/main.12b53443.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "509918b2b60b25cd2d683f9963f0f59a",
+    "url": "/portfolio/static/media/banner.509918b2.gif"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/portfolio/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/portfolio/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/portfolio/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/portfolio/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
